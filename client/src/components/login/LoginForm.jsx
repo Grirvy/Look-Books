2920dfcc902263506e4df009ce253b902c9ca168
Isiarf <imrfauntleroy@yahoo.com>
2024-03-14 03:25:00 -0700

    moved files into named folders on the client side so they are easier to find.

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -2,8 +2,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { loginUser } from '../../utils/API';
+import Auth from '../../utils/authentication/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
